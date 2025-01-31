f54183fffacbcc41a931ff176de23602147d9388
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-30 19:15:03 -0500

    feat: add exercise 14

---
+++ 14.exercise.js
@@ -0,0 +1,12 @@
+// 14. Sort an Array
+
+// - Write a function that takes an array of numbers and sorts it in ascending order.
+// - Example: sortArray([3, 1, 4, 1, 5]) -> [1, 1, 3, 4, 5]
+
+const sortArray = (arr) => {
+  let result = arr.sort()
+
+  return result
+}
+
+console.log(sortArray([3, 1, 4, 1, 5]))
